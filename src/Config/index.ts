2d32fcd68a8ae2e10aae5d9221af0306e30c06e6
Conner Ruhl <connerruhl@carfax.com>
2018-06-04 16:09:10 -0500

    Got relative paths working for tsconfig inheritance

---
+++ src/Config/index.ts
@@ -0,0 +1,9 @@
+import tsconfig from "./tsconfig.json";
+import tslint from "./tslint.json";
+import devDependencies from "./devDependencies.json";
+
+export default {
+  tsconfig,
+  tslint,
+  devDependencies
+};
