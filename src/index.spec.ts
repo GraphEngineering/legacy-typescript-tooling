ec09e9f8c41c9066b4361734f7f9b64801bcdf31
Conner Ruhl <connerruhl@carfax.com>
2018-06-06 13:31:35 -0500

    Adding tests for devDependencies

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,32 @@
+import { extendDefaultJestConfig } from "./index";
+
 import * as Config from "~/Config";
 
-test("if jest is working", () => {
-  console.log(Config);
+// describe("configuring typescript", () => {
+//   // test("user config is merged with defaults", () => {
 
-  expect(1 + 1).toBe(2);
+//   // });
+// });
+
+describe("managing `devDependencies`", () => {
+  test("user `devDependencies` are merged with defaults", () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe("configuring jest", () => {
+  test("user provided config is merged with defaults", () => {
+    const userConfig = {
+      verbose: true,
+      moduleFileExtensions: ["scss"]
+    };
+
+    const mergedConfig = extendDefaultJestConfig(userConfig);
+
+    expect(mergedConfig.verbose).toBe(true);
+    expect(mergedConfig.moduleFileExtensions).toEqual([
+      ...Config.jest.moduleFileExtensions,
+      ...userConfig.moduleFileExtensions
+    ]);
+  });
 });
