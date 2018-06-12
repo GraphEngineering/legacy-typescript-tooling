2890991b4405491089f4aaac85615ef4e3a9fc6a
Conner Ruhl <connerruhl@carfax.com>
2018-06-12 15:00:04 -0500

    Added pretty console logs

--- src/configureProject.ts
+++ src/configureProject.ts
@@ -1,8 +1,16 @@
 import * as fs from "fs";
 import * as path from "path";
+
 import merge from "deepmerge";
 
+import * as log from "./log";
 import * as DefaultConfigs from "./DefaultConfigs";
+
+const DEFAULT_CONFIGS_PATH =
+  "./node_modules/typescript-tooling/dist/DefaultConfigs/";
+
+const JEST_CONFIG_FILE_NAME = "jest.config.js";
+const JEST_CONFIG_FILE_CONTENTS = `module.exports = require("typescript-tooling").extendWithDefaultJestConfig({});\n`;
 
 export const copyDevDependenciesToPackageJSON = () => {
   const packageJSONPath = pathForConfigFile("package.json");
@@ -22,36 +30,58 @@ export const copyDevDependenciesToPackageJSON = () => {
     packageJSONPath,
     JSON.stringify(packageJSONContentsWithDevDependencies, null, 2)
   );
+
+  console.info(
+    `${log.checkMark} Copied default ${log.fileName(
+      "devDependencies"
+    )} to ${log.fileName("package.json")}${log.code(
+      JSON.stringify(DefaultConfigs.devDependencies, null, 2)
+    )}`
+  );
 };
 
 export const createOrExtendTSConfigFileJSON = (fileName: string) => {
   const TSConfigFilePath = pathForConfigFile(fileName);
 
-  const TSConfigFileSettings = fs.existsSync(TSConfigFilePath)
-    ? JSON.parse(fs.readFileSync(TSConfigFilePath).toString())
-    : {};
+  const [consoleInfoVerb, TSConfigFileContents] = fs.existsSync(
+    TSConfigFilePath
+  )
+    ? ["Extended", JSON.parse(fs.readFileSync(TSConfigFilePath).toString())]
+    : ["Created", {}];
 
-  const TSConfigFileSettingsWithExtends = {
-    extends: `./node_modules/typescript-tooling/dist/DefaultConfigs/${fileName}`,
-    ...TSConfigFileSettings
+  const TSConfigFileContentsWithExtends = {
+    extends: `${DEFAULT_CONFIGS_PATH}${fileName}`,
+    ...TSConfigFileContents
   };
 
-  fs.writeFileSync(
-    TSConfigFilePath,
-    JSON.stringify(TSConfigFileSettingsWithExtends, null, 2)
+  const TSConfigFileSettingsWithExtendsJSON = JSON.stringify(
+    TSConfigFileContentsWithExtends,
+    null,
+    2
+  );
+
+  fs.writeFileSync(TSConfigFilePath, TSConfigFileSettingsWithExtendsJSON);
+
+  console.info(
+    `${log.checkMark} ${consoleInfoVerb} ${log.fileName(fileName)}${log.code(
+      TSConfigFileSettingsWithExtendsJSON
+    )}`
   );
 };
 
 export const createDefaultJestConfigJS = () => {
-  const jestConfigJSPath = pathForConfigFile("jest.config.js");
+  const jestConfigJSPath = pathForConfigFile(JEST_CONFIG_FILE_NAME);
 
   if (fs.existsSync(jestConfigJSPath)) {
     return;
   }
 
-  fs.writeFileSync(
-    jestConfigJSPath,
-    `module.exports = require("typescript-tooling").extendWithDefaultJestConfig({});\n`
+  fs.writeFileSync(jestConfigJSPath, JEST_CONFIG_FILE_CONTENTS);
+
+  console.info(
+    `${log.checkMark} created ${log.fileName("jest.config.js")}${log.code(
+      JEST_CONFIG_FILE_CONTENTS
+    )}`
   );
 };
 
--- src/executeConfigurationSteps.ts
+++ src/executeConfigurationSteps.ts
@@ -4,9 +4,15 @@ import {
   createDefaultJestConfigJS
 } from "./configureProject";
 
+import { default as chalk } from "chalk";
+
+console.info(chalk.bold.green("Making sure TypeScript is ready-to-go!"));
+
 copyDevDependenciesToPackageJSON();
 
 createOrExtendTSConfigFileJSON("tsconfig.json");
 createOrExtendTSConfigFileJSON("tslint.json");
 
 createDefaultJestConfigJS();
+
+console.info(chalk.bold.green("All set!"));
