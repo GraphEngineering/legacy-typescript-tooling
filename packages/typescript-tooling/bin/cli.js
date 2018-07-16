3528ec6cd55c4f6b5d4c09de38917f39ae755238
Conner Ruhl <connerruhl@carfax.com>
2018-07-16 16:28:04 -0500

    Switching to monorepo style

--- packages/typescript-tooling/bin/cli.js
+++ packages/typescript-tooling/bin/cli.js
@@ -1,6 +1,31 @@
 #! /usr/bin/env node
 
-const { version } = require("../package.json");
-const { main } = require("../dist/index");
+// const { version } = require("../package.json");
+// const { main } = require("../dist/index");
 
-main(version, process.argv);
+// main(version, process.argv);
+
+const exec = require("child_process").exec;
+
+const [, npmScriptName] = JSON.parse(process.env.npm_config_argv).original;
+const [packageName, scriptName, scriptModifier] = npmScriptName.split(":");
+
+console.log({ packageName, scriptName, scriptModifier });
+
+const command = {
+  clean: `rimraf packages/${packageName}/dist`
+}[scriptName];
+
+const childProcess = exec(command);
+
+childProcess.stdout.on("data", data => {
+  process.stdout.write(data);
+});
+
+childProcess.on("error", data => {
+  process.exit(1);
+});
+
+childProcess.on("exit", () => {
+  process.exit(0);
+});
