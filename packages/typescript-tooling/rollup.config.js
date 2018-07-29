a6a0ecd9d71907de9fb0afe6f7c1acdac6f7588b
Conner Ruhl <connerruhl@me.com>
2018-07-29 17:47:01 -0500

    Add comments in the default config files to help explain why certain settings are used

--- packages/typescript-tooling/rollup.config.js
+++ packages/typescript-tooling/rollup.config.js
@@ -6,29 +6,40 @@ import typescript from "rollup-plugin-typescript2";
 
 export default {
   plugins: [
+    // make it possible to direcly import static JSON files
     json(),
+
+    // automatically bring in CommonJS imports and use node module resolution
     commonjs(),
     resolve(),
+
+    // enable source maps
     sourceMaps(),
+
+    // configure TypeScripts with overrides for compiler settings
     typescript({
       tsconfigOverride: {
+        // inform the compiler of any module types we handle with Rollup (JSON)
         include: [".tst/declarations.d.ts"],
+
+        // force the compiler to leave JSON imports as-is, we handle them above
         compilerOptions: { resolveJsonModule: false }
       }
     })
   ],
 
-  input: "packages/twilio/src/index.ts",
+  // use source maps and CommonJS for output
   output: {
-    file: "packages/twilio/dist/index.js",
     format: "cjs",
     sourcemap: true
   },
 
+  // don't worry about node's built-in modules (e.g. "fs", "path", "http")
   external: Object.keys(process.binding("natives")).filter(
     module => !/^_|^(internal|v8|node-inspect)\/|\//.test(module)
   ),
 
+  // suppress errors caused by the way TypeScript generates JavaScript
   onwarn: (warning, warn) =>
     warning.code !== "THIS_IS_UNDEFINED" && warn(warning)
 };
